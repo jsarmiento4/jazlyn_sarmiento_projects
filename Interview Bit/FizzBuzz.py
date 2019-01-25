787777e183007804dd5a3052ba14336c5238a0a4
Jazz <jsarmiento@csumb.edu>
2019-01-25 22:23:17 +0000

    fizzbuzz3.0

--- Interview Bit/FizzBuzz.py
+++ Interview Bit/FizzBuzz.py
@@ -1,15 +1,16 @@
-def Fizzbuzz(n):
-  result = []
+class Solution:
+    def Fizzbuzz(n):
+        result = []
 
-  for i in range(1, n + 1):
-    if i % 3 == 0 and i % 5 == 0:
-      result.append("Fizzbuzz")
-    elif i % 3 == 0:
-      result.append("Fizz")
-    elif i % 5 == 0:
-      result.append("Buzz")
-    else:
-      result.append(i)
-  return result
+        for i in range(1, n + 1):
+            if i % 3 == 0 and i % 5 == 0:
+                result.append("Fizzbuzz")
+            elif i % 3 == 0:
+                result.append("Fizz")
+            elif i % 5 == 0:
+                result.append("Buzz")
+            else:
+                result.append(i)
+        return result
 
-print Fizzbuzz(15)
+    print Fizzbuzz(15)
